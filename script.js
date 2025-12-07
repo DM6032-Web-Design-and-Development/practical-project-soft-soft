ebc38dbba32cb37d9f2a5f6719986a35d983a2be
tigerskull444-stack <tigerskull444@gmail.com>
2025-12-07 12:32:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,10 @@ function showTab(n) {
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
 
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
+  document.getElementById("prevBtn").style.display = n == 0 ? "none" : "inline";
 
-  if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
+  document.getElementById("nextBtn").innerHTML =
+    n == x.length - 1 ? "Submit" : "Next";
 
   fixStepIndicator(n);
 }
@@ -31,7 +24,8 @@ function nextPrev(n) {
   if (n == 1 && !validateForm()) return false;
 
   x[currentTab].style.display = "none";
-  currentTab = currentTab + n;
+
+  currentTab += n;
 
   if (currentTab >= x.length) {
     document.getElementById("regForm").submit();
@@ -42,30 +36,28 @@ function nextPrev(n) {
 }
 
 function validateForm() {
-  var x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
+  var x = document.getElementsByClassName("tab");
+  var y = x[currentTab].querySelectorAll("input, textarea");
+  var valid = true;
 
-  for (i = 0; i < y.length; i++) {
-    if (y[i].value == "") {
-      y[i].className += " invalid";
+  y.forEach(el => {
+    if (el.value === "") {
+      el.classList.add("invalid");
       valid = false;
     }
-  }
+  });
 
   if (valid) {
     document.getElementsByClassName("step")[currentTab].classList.add("finish");
   }
 
   return valid;
-} 
+}
 
 function fixStepIndicator(n) {
-  var i, x = document.getElementsByClassName("step");
-  
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
+  var x = document.getElementsByClassName("step");
+  for (var i = 0; i < x.length; i++) {
+    x[i].classList.remove("active");
   }
-
-  x[n].className += " active";
+  x[n].classList.add("active");
 }
